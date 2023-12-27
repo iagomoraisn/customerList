ea55fc99ffc2e9967ca701e04cccc8ba639a1df1
iagomoraisn <iagojkd96@hotmail.com>
2023-12-27 09:54:49 -0300

    atlasMongoDb

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import Fastify from "fastify";
 import { routes } from "./routes";
-// import cors from "@fastify/cors"
+import cors from "@fastify/cors"
 
 const app = Fastify({ logger:true })
 
 const start = async () => {
 
+    await app.register(cors);
     await app.register(routes);
 
     try{
